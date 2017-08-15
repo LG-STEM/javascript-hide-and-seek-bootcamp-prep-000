dcb600c6c006e2c84283721de9a8d9cc3e7d8bef
LG-STEM <gatesinfo@gmail.com>
2017-08-15 13:39:12 +0000

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-
+  return document.querySelector('#nested .target')
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lis = document.querySelectorAll('.ranked-list')
 
   for (var i = 0; i < lis.length; i++) {
-    lis[i].innerHTML += 1
+    var children = lis[i].children
+
+    for (var j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
+}
+
+function deepestChild() {
+  var node = document.getElementById('grand-node')
+  var nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
